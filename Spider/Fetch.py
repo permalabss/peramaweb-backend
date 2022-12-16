206c6cbe1e58e734a21f285d9adf423b0cc6595d
yyx <775289630@qq.com>
2022-12-17 01:14:17 +0800

    add health

--- Spider/Fetch.py
+++ Spider/Fetch.py
@@ -16,6 +16,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/health', methods=["GET"])
+def health():
+    return "ok"
 
 @app.route('/fetchurl', methods=["GET"])
 def fetchHtml():
